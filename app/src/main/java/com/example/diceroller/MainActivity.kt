1e53ffef34e360b44dd8de7246f254b8ddff5ccf
Bulat <@LAPTOP-SSS2TI2M>
2023-10-29 17:05:01 +0300

    Project ready

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -48,18 +48,25 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    val imageResource = R.drawable.dice_1
-
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_1
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(imageResource),
-            contentDescription = ""
+            contentDescription = result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {  }) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
